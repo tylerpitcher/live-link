59f310097a27359311e0db8d9bbf217d8d67d8f8
Tyler Pitcher <20trlp@queensu.ca>
2024-09-29 13:57:01 -0400

    Update backend cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,10 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 
 app.use('/api/graphql', require('./routes/graphqlRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
